edda1b3e4d78dc4aa2ccd47872ca4d5c313cc588
Antika <antikasaha15@gmail.com>
2020-10-12 20:30:44 +0600

    Passing Data & Creating Rooms events

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const socketIO = require('socket.io');
 
 const {generateMessage, generateLocationMessage} = require('./utils/message');
+const {isRealString} = require('./utils/isRealString');
 const publicPath = path.join(__dirname,'/../public');
 const port = process.env.PORT || 3000;
 
@@ -15,8 +16,16 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket) =>{
     console.log("A new user just connected");
-    socket.emit('newMessage',generateMessage('Admin','Welcome to the chat app!'));
-    socket.broadcast.emit('newMessage',generateMessage('Admin','New User joined!'));
+
+    socket.on('join',(params,callback) =>{
+       if(!isRealString(params.name) || !isRealString(params.room)){
+           callback('Name and room are required');
+       }
+       socket.join(params.room);
+        socket.emit('newMessage',generateMessage('Admin','Welcome to the chat app!'));
+        socket.broadcast.emit('newMessage',generateMessage('Admin','New User joined!'));
+       callback();
+    })
     socket.on('createMessage',(message,callback)=>{
         console.log("createMessage",message);
             io.emit('newMessage',generateMessage(message.from,message.text));
---
+++ server/utils/validation.test.js
@@ -0,0 +1,22 @@
+const expect = require('expect');
+
+const {isRealString} = require('./isRealString');
+
+describe('is Real String',() =>{
+    it('should reject non-string values',() =>{
+      let res = isRealString(65);
+      expect(res).toBe(false);
+    });
+
+    it('should reject non-string with only spaces',() =>{
+        let res = isRealString('              ');
+        expect(res).toBe(false);
+    });
+
+    it('should allow string with non-space chars',() =>{
+        let res = isRealString('    Chat            ');
+        expect(res).toBe(true);
+
+    });
+
+})
